cc1883af8cf6bc4cac81482194f5c61021d21175
tyanogi <tyanogi@gmail.com>
2016-03-19 22:19:50 +0900

    Add request.php
    postの処理

--- system/request.php
+++ system/request.php
@@ -2,9 +2,6 @@
 
 require_once('./require.php');
 
-// インスタンスの生成
-$authInstance = new SYS_Auth();
-
 // POSTの受け取り
 $requestType = htmlspecialchars($_POST['requestType'], ENT_QUOTES, 'UTF-8');
 if(isset($_POST['nextUrl'])) {
@@ -18,12 +15,9 @@ if(isset($_POST['token'])) {
 // リクエストに応じてPOSTを振り分ける
 switch($requestType) {
     ### administrator request ###
-    // サーバ起動時の設定
-    case 'admin_setup_server':
-        break;
-    // データベースの初期化
-    case 'admin_initialize_database':
-        break;
+    case 'placeinfo':
+        $inst = new SYS_Placeinfo();
+        $inst->addPlaceinfo($_POST);
     // フロントの設定
     case 'admin_edit_basic':
         $authInstance->typeCheck(array(10));
@@ -47,82 +41,6 @@ switch($requestType) {
         $authInstance->typeCheck(array(10));
         $inst = new SYS_Team();
         $inst->updateTeam($_POST);
-        break;
-
-    ### author request ###
-    // 問題の追加
-    case 'author_question_add':
-        $authInstance->typeCheck(array(10, 11));
-        $inst = new SYS_Question();
-        $inst->setPost($_POST);
-        break;
-    // 問題の編集
-    case 'author_question_edit':
-        $authInstance->typeCheck(array(10, 11));
-        $inst = new SYS_Question();
-        $inst->updateQuestion($_POST);
-        break;
-    // 問題の削除
-    case 'author_question_remove':
-        break;
-    // ジャンルの追加
-    case 'author_add_genre':
-        $authInstance->typeCheck(array(10, 11));
-        $inst = new SYS_Genre();
-        $inst->addGenre($_POST);
-        break;
-    case 'author_edit_genre':
-        $authInstance->typeCheck(array(10, 11));
-        $inst = new SYS_Genre();
-        $inst->updateGenre($_POST);
-        break;
-    case 'author_remove_genre':
-        break;
-
-    ### user request ####
-    // ユーザ・チームの登録
-    case 'user_join':
-        $inst = new SYS_Join();
-        $logInstance = new SYS_Log();
-        $logInstance->user_join($_POST);
-        $inst->setPost($_POST);
-        break;
-    // ユーザのログイン処理
-    case 'user_login':
-        $inst = new SYS_Login();
-        $logInstance = new SYS_Log();
-        $result = $inst->setPost($_POST);
-        $logInstance->user_login($_POST, $result);
-        break;
-    // ユーザのログアウト処理
-    case 'user_logout':
-        $logInstance = new SYS_Log();
-        $logInstance->user_logout();
-        $authInstance->logout();
-        break;
-    // ユーザによるユーザ編集
-    case 'user_edit_userinfo':
-        $authInstance->loginCheck();
-        $inst = new SYS_EditUser();
-        $inst->setUserInfo($_POST);
-        break;
-    // ユーザによるパスワード編集
-    case 'user_edit_userpw':
-        $authInstance->loginCheck();
-        $inst = new SYS_EditUser();
-        $inst->setUserPW($_POST);
-        break;
-    // ユーザによるチーム編集
-    case 'user_edit_teaminfo':
-        $authInstance->loginCheck();
-        $inst = new SYS_EditTeam();
-        $inst->setTeamInfo($_POST);
-        break;
-    // ユーザによるチームパスワード編集
-    case 'user_edit_teampw':
-        $authInstance->loginCheck();
-        $inst = new SYS_EditTeam();
-        $inst->setTeamPW($_POST);
         break;
 
     ### other request ###
