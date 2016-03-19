901a02de85f8b936283e54ce530b63fba5a6873e
doukeshiDONALDO <b1401730@planet.kanazawa-it.ac.jp>
2016-03-20 08:06:51 +0900

    アイコンの説明部分を画像が表示できるようにした

--- view.php
+++ view.php
@@ -345,14 +345,14 @@ function deleteMarker__(x)
                                 </div>
                                 <div class="panel-body">
     　　　　　　　　　　　　　　　　　　　　　　　　　 <ul class="list-inline">
-                                     <li class="icon-desc"><img src="./image/icons/2.png" width="38px" height="38px"><span class="desc">AED</span></li>
                                      <li class="icon-desc"><img src="./image/icons/1.png" width="38px" height="38px"><span class="desc">避難所</span></li>
+                                     <li class="icon-desc"><img src="./image/icons/2.png" width="38px" height="38px"><span class="desc">AED</span></li>
                                      <li class="icon-desc"><img src="./image/icons/3.png" width="38px" height="38px"><span class="desc">110番の家</span></li>
-                                     <li class="icon-desc"><img src="./image/icons/7.png" width="38px" height="38px"><span class="desc">車に注意</span></li>
-                                     <li class="icon-desc"><img src="./image/icons/8.png" width="38px" height="38px"><span class="desc">用水に注意</span></li>
-                                     <li class="icon-desc"><img src="./image/icons/9.png" width="38px" height="38px"><span class="desc">踏切注意</span></li>
-                                     <li class="icon-desc"><img src="./image/icons/10.png" width="38px" height="38px"><span class="desc">夜道が暗い</span></li>
-                                     <li class="icon-desc"><img src="./image/icons/11.png" width="38px" height="38px"><span class="desc">見通しが悪い</span></li>
+                                     <li class="icon-desc"><img src="./image/icons/4.png" width="38px" height="38px"><span class="desc">車に注意</span></li>
+                                     <li class="icon-desc"><img src="./image/icons/5.png" width="38px" height="38px"><span class="desc">用水に注意</span></li>
+                                     <li class="icon-desc"><img src="./image/icons/6.png" width="38px" height="38px"><span class="desc">踏切注意</span></li>
+                                     <li class="icon-desc"><img src="./image/icons/7.png" width="38px" height="38px"><span class="desc">夜道が暗い</span></li>
+                                     <li class="icon-desc"><img src="./image/icons/8.png" width="38px" height="38px"><span class="desc">見通しが悪い</span></li>
                                    </ul>
   　　　　　　　　　　　　　　　　　　　　　　　　　</div>
                                </div>
